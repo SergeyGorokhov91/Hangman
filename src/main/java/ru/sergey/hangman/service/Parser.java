4519abdf7d68c421720353d6ae88218e28c3a51a
SergeyGorokhov91 <108141894+SergeyGorokhov91@users.noreply.github.com>
2023-01-16 13:06:41 +0300

    Add auto-create db and parse word from resoures folder

--- src/main/java/ru/sergey/hangman/service/Parser.java
+++ src/main/java/ru/sergey/hangman/service/Parser.java
@@ -18,19 +18,20 @@ public class Parser {
     public void textFileToDBSaveLogic() {
     	if(wordRepo.findById((long) 1).isEmpty()) {
 	        try {
-	            File file = new File("C:\\Users\\Сергей\\Desktop\\russian_nouns.txt");
-	            FileReader fileReader = new FileReader(file);
-	            BufferedReader reader = new BufferedReader(fileReader);
-	            String line = reader.readLine();
-	            while(line != null) {
-	                Word word = new Word(line);
-	                if(word.getWord().indexOf('-') == -1) {
-	                	String newStr = word.getWord().replaceAll("ё", "е");
-	                	word.setWord(newStr);
-		                wordRepo.save(word);
-	                }
-	                line = reader.readLine();
-	            }
+	        	ClassLoader classLoader = getClass().getClassLoader();
+	        	File file = new File(classLoader.getResource("russian_nouns.txt").getFile());
+	        	try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+					String line = reader.readLine();
+					while(line != null) {
+					    Word word = new Word(line);
+					    if(word.getWord().indexOf('-') == -1) {
+					    	String newStr = word.getWord().replaceAll("ё", "е");
+					    	word.setWord(newStr);
+					        wordRepo.save(word);
+					    }
+					    line = reader.readLine();
+					}
+				}
 	        } catch (FileNotFoundException e) {
 	            e.printStackTrace();
 	        } catch (IOException e) {
